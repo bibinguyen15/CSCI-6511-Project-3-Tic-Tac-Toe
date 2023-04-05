d667b3ddb616e715e006126d5bdd4f86892fedcb
Cat <bibispamemails@gmail.com>
2023-04-05 17:27:54 -0400

    Miscel

--- Project 3 Cat.py
+++ Project 3 Cat.py
@@ -4,6 +4,11 @@ class TTT:
         self.target = target
         self.players = ['O', 'X']
         self.board = [['-'] * boardSize] * boardSize
+        self.available = []
+        for x in range(boardSize):
+            for y in range(boardSize):
+                self.available.append([x, y])
+        print(self.available)
 
     def drawBoard(self):
         for row in self.board:
@@ -16,14 +21,27 @@ class TTT:
 
     def setBoard(self, board):
         self.board = board.split('\n')[:-1]
+        for i in range(len(self.board)):
+            self.board[i] = [*self.board[i]]
+        print(self.board)
         self.boardSize = len(self.board)
         self.target = int(self.boardSize / 2)
+
+    def heu(self):
+        return 1
+
+    def next(self, current):
+        return 0
+
+    def win(self):
+        winner = ''
+        for row in self.board:
 
 
 def main():
     game = TTT(3, 3)
 
-    game.setBoard("O-----------\nOO----------\n---O--------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n")
+    # game.setBoard("O-----------\nOO----------\n---O--------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n")
     game.drawBoard()
 
 
