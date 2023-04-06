7e657b4e6b48450196c7a6d86595ad8d1e456ec4
Cat <catherinehnguyen15@gmail.com>
2023-04-05 20:41:22 -0400

    Transfer

--- Project 3 Cat.py
+++ Project 3 Cat.py
@@ -19,13 +19,16 @@ class TTT:
             print()
         print(self.boardSize, self.target)
 
-    def setBoard(self, board):
+    def setBoard(self, board, target=-1):
         self.board = board.split('\n')[:-1]
         for i in range(len(self.board)):
             self.board[i] = [*self.board[i]]
         print(self.board)
         self.boardSize = len(self.board)
-        self.target = int(self.boardSize / 2)
+        if target == -1:
+            self.target = int(self.boardSize / 2)
+        else:
+            self.target = target
 
     def heu(self):
         return 1
@@ -35,14 +38,57 @@ class TTT:
 
     def win(self):
         winner = ''
+        adjacent = [self.board[0][0], 0]
+        
+        
+        #First check row
         for row in self.board:
+            adjacent = [row[0], 0]
+            for item in row:
+                #if match
+                if item == adjacent[0] and item != '-':
+                    adjacent[1] += 1
+                    print(adjacent)
+                    if adjacent[1] == self.target:
+                        return item
+                else:
+                    adjacent = [item, 1]
+                    
+                    
+        #Check column
+        for j in range(self.boardSize):
+            adjacent = [self.board[0][j], 0]
+            for i in range(self.boardSize):
+                if self.board[i][j] == adjacent[0] and self.board[i][j] != '-':
+                    adjacent[1] += 1
+                    if adjacent[1] == self.target:
+                        return self.board[i][j]
+                else:
+                    adjacent = [self.board[i][j], 1]
+        
+        #Check diagonal
+        for i in range(self.boardSize):
+            for j in range(self.boardSize):
+                adjacent = []
+                
+                
+        #Check tie
+        if len(self.available) == 0:
+            return True
+               
+                    
+        
+        
+        
 
 
 def main():
     game = TTT(3, 3)
 
+    game.setBoard("---\n-OX\n-OX\n", 3)
     # game.setBoard("O-----------\nOO----------\n---O--------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n------------\n")
     game.drawBoard()
+    print("Winner is:", game.win())
 
 
 if __name__ == '__main__':
