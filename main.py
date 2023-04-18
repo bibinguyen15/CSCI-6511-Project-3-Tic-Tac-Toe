b873da3aef875a95c19cbc2de0117aa0f48eea61
Cat <bibispamemails@gmail.com>
2023-04-18 19:31:19 -0400

    Added local play with user only

--- main.py
+++ main.py
@@ -11,16 +11,91 @@ user = 0
 
 
 def main():
+    #localPlayPerson(boardSize, target)
 
-    # localPlay()
+    # localPlayAI()
     teamId2 = '1349'
-    gameId = 0
+    gameId = 4272
     opponentStarts = False
 
     loadGame(gameId, teamId2, opponentStarts)
 
 
-def localPlay():
+def localPlayPerson(size, target, playerStart=False):
+
+    print("Starting game...")
+
+    if playerStart:
+        user, opponent = 1, 0
+    else:
+        user, opponent = 0, 1
+
+    # Just putting stock values for size and target because it will be override by setBoard anyways
+    board = Board(size, target, user)
+
+    board.drawBoard()
+
+    board.print()
+
+    # if player start first
+    if playerStart:
+        x, y = int(input("x=")), int(input("y="))
+
+        board.add([x, y], opponent)
+        board.drawBoard()
+
+    while True:
+        board.print()
+
+        bestMove = setMove(board)
+
+        board.add(bestMove, user)
+        board.print()
+
+        print("AI move:")
+        board.drawBoard()
+
+        # board.print()
+
+        if board.gameOver() or board.isFull():
+            print("Game ended.\nWinner:", board.getWinner())
+            # return
+
+        x, y = int(input("x=")), int(input("y="))
+
+        board.add([x, y], opponent)
+
+        print("Opponent move:")
+
+        board.drawBoard()
+
+        # board.print()
+
+
+def setMove(board):
+    available = len(board.available())
+
+    print("Game ID is:", gameId)
+
+    if available > 30:
+        constants.maxDepth = 2
+    elif available > 25:
+        constants.maxDepth = 3
+    elif available > 20:
+        constants.maxDepth = 4
+    elif available > 10:
+        constants.maxDepth = 5
+    else:
+        constants.maxDepth = 10
+
+    bestMove = nextMove(board, board.getUser())
+    board.print()
+    print("Best move is:", bestMove)
+
+    return bestMove
+
+
+def localPlayAI():
 
     game = Board(boardSize, target, user)
 
